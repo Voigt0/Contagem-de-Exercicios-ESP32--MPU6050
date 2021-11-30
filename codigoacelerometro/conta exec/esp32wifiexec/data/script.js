feaca51e3c59d9ed7825c5e7916476d477527cee
Voigt0 <voigtrodrigo0@gmail.com>
2021-11-30 10:04:26 -0300

    Create script.js

---
+++ codigoacelerometro/conta exec/esp32wifiexec/data/script.js
@@ -0,0 +1,58 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-mpu-6050-web-server/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// Create events for the sensor readings
+if (!!window.EventSource) {
+  var source = new EventSource('/events');
+
+  source.addEventListener('open', function(e) {
+    console.log("Events Connected");
+  }, false);
+
+  source.addEventListener('error', function(e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
+
+  source.addEventListener('gyro_readings', function(e) {
+    //console.log("gyro_readings", e.data);
+    var obj = JSON.parse(e.data);
+    document.getElementById("gyroX").innerHTML = obj.gyroX;
+    document.getElementById("gyroY").innerHTML = obj.gyroY;
+    document.getElementById("gyroZ").innerHTML = obj.gyroZ;
+
+    // Change cube rotation after receiving the readinds
+    cube.rotation.x = obj.gyroY;
+    cube.rotation.z = obj.gyroX;
+    cube.rotation.y = obj.gyroZ;
+    renderer.render(scene, camera);
+  }, false);
+
+  source.addEventListener('temperature_reading', function(e) {
+    console.log("temperature_reading", e.data);
+    document.getElementById("temp").innerHTML = e.data;
+  }, false);
+
+  source.addEventListener('accelerometer_readings', function(e) {
+    console.log("accelerometer_readings", e.data);
+    var obj = JSON.parse(e.data);
+    document.getElementById("accX").innerHTML = obj.accX;
+    document.getElementById("accY").innerHTML = obj.accY;
+    document.getElementById("accZ").innerHTML = obj.accZ;
+    document.getElementById("accA").innerHTML = obj.accA;
+    document.getElementById("numEx").innerHTML = obj.numEx;
+  }, false);
+}
+
+function resetPosition(element){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/"+element.id, true);
+  console.log(element.id);
+  xhr.send();
+}
